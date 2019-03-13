15bc5e97ce6a06ac9893ef484800d5beeacb304d
Waheed Brown <waheedbrown@gmail.com>
2019-03-12 20:19:59 -0700

    Initial commit

---
+++ 3_Project_Boost/Assets/Scripts/Orbital Platform/LivingQuarters.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LivingQuarters : MonoBehaviour
+{
+    enum FrameRate : int { FPS30 = 30, FPS60 = 60, FPS80 = 80 };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Time.deltaTime * (int)FrameRate.FPS30);
+    }
+}
---
+++ 3_Project_Boost/Assets/Scripts/Space Shuttle/SpaceShuttle.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShuttle : MonoBehaviour
+{
+    // Movement
+    bool ignition = false;
+    Rigidbody rigidBody;
+    enum FrameRate : int { FPS30 = 30, FPS60 = 60, FPS80 = 80 };
+
+    // Audio
+    AudioSource audioSource;
+    float defaultVolume;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        RemoveSoundArtifacts();
+    }
+
+    // Unity has a bug where stopping as sound causes a "popping" sound
+    // To remove this artifact:
+    // 1. use a 2 minute thruster sound clip
+    // 2. never stop the sound
+    // 3. reduce sound to 0.001 instead of stopping it
+    private void RemoveSoundArtifacts()
+    {
+        defaultVolume = audioSource.volume;
+        audioSource.volume = 0.001f;
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rotate();
+        Thrust();
+    }
+
+    private void Rotate()
+    {
+        if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D))
+        {
+            // Disable rotation of both A and D are pressed
+            print("Disable rotation");
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * Time.deltaTime * (int)FrameRate.FPS60);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * Time.deltaTime * (int)FrameRate.FPS60);
+        }
+    }
+
+    private void Thrust()
+    {
+        if (ignition == true)
+        {
+            rigidBody.useGravity = true;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+            ignition = true;
+        }
+        ControlThurstSound();
+    }
+
+    private void ControlThurstSound()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            audioSource.volume = defaultVolume;
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            audioSource.volume = 0.001f;
+        }
+    }
+}
